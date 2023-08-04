97cb0611b7a17267b958553f5114eea95769263a
abrilxcx <abrlbjrn@gmail.com>
2023-08-03 19:32:31 -0600

    183. Array With

--- 01-arrays/array-with.js
+++ 01-arrays/array-with.js
@@ -1,4 +1,4 @@
-const superHeroes = [
+const state = [
    {
       id: 1,
       name: 'Batman',
@@ -17,9 +17,26 @@ const superHeroes = [
    },
 ]
 
-const superHeroesCopy = structuredClone( superHeroes );
+const index = 1;
+const newName = 'Green Lantern';
 
-superHeroesCopy[0].name = 'Green Lantern';
+// const newState = state.map( (hero, i) => {
 
-console.table( superHeroes );
-console.table( superHeroesCopy );
+//    if( i === index ) {
+//       hero.name = newName;
+//    }
+
+//    return {...hero};
+
+// });
+
+const newState = state.with( index, {
+   ...state.at(index),
+   name: newName,
+});
+
+state[0].name = 'Volcan Negro';
+
+console.table(newState);
+
+console.log('El último: ', state.at(-2));
--- main.js
+++ main.js
@@ -2,6 +2,6 @@ import './style.css';
 import './01-arrays/array-with';
 
 document.querySelector('#app').innerHTML = `
-  <h1>hola</h1>
+  <h1>🐕‍🦺</h1>
 `
 
